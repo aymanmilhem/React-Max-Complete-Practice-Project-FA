6e1d5109ba50872df8379b699ba73469b47fdcf1
Ayman Milhem <aymanmilhem@gmail.com>
2022-07-18 20:06:36 +1200

    first commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AddUser = props => {
+    const addUserHanlder = (event) => {
+        event.preventDefault();
+    }
+
+
+    return (
+        <form onSubmit={addUserHanlder}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text"/>
+            <label htmlFor="age">Age</label>
+            <input id="age" type="number" />
+            <button type="submit">Add User</button>
+        </form>
+    )
+ };
+
+export default AddUser;
